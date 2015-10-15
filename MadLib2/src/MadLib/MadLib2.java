a3766e15d0988b52740381a2892bd0d0413a6919
GBTC440012UR <GBTC440012UR@GBTC363I12W7.wdce.local>
2015-10-15 09:30:43 -0400

    first commit

---
+++ MadLib2/src/MadLib/MadLib2.java
@@ -0,0 +1,12 @@
+package MadLib;
+import java.net.URL;
+import java.util.Scanner;
+import java.util.Random;
+public class MadLib2 
+{
+	public static void main(String[] args) throws Exception
+	{
+		MovieTitleGenerator.generate();
+	}
+
+}
